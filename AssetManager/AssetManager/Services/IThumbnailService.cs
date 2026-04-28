a175ab60c857cf08b3c7f7a9103f2211738a5657
zyt <74599950+Zzzsix@users.noreply.github.com>
2026-04-28 15:07:35 +0800

    Delete AssetManager directory

--- AssetManager/AssetManager/Services/IThumbnailService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows.Media.Imaging;
-
-namespace AssetManager.Services
-{
-    public interface IThumbnailService
-    {
-        BitmapSource? GetThumbnail(string filePath);
-    }
-}
